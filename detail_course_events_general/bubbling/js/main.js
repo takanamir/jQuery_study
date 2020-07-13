caf61ccd23ac33064ce7fb03e50dd51cf935f0b5
Ryo Takanami <rtakanami0706gmail.com>
2020-07-13 17:31:41 +0900

    イベント(汎用操作)～CSS

---
+++ detail_course_events_general/bubbling/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+	// クリック時の処理
+	$("*").click(function() {
+		var tag = this.tagName;
+		var id = this.id;
+		console.log("click", tag, id);
+		// return false; // バブリングをブロック
+	});
+});
